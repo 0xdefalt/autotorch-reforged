75940689c671fba0e08c2031219cef6571c19d61
0xdefalt <erickhax@icloud.com>
2026-02-22 14:37:49 -0300

    feat: port to 1.21.11

--- src/main/java/autotorch/autotorch/client/AutotorchClient.java
+++ src/main/java/autotorch/autotorch/client/AutotorchClient.java
@@ -1,19 +1,3 @@
-//Autotorch: A fabric mod to automatically place torches in offhand
-//Copyright (C) 2021 Shamil K
-
-//This program is free software: you can redistribute it and/or modify
-//it under the terms of the GNU Lesser General Public License as published by
-//the Free Software Foundation, either version 3 of the License, or
-//(at your option) any later version.
-
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU Lesser General Public License for more details.
-
-//You should have received a copy of the GNU Lesser General Public License
-//along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 package autotorch.autotorch.client;
 
 import com.google.common.collect.ImmutableSet;
@@ -21,53 +5,52 @@ import com.google.common.collect.ImmutableSet;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.block.Block;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.math.Direction;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.block.Block;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.LightType;
-import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
-
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.ConfigHolder;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import org.lwjgl.glfw.GLFW;
 
-
 @Environment(EnvType.CLIENT)
-public class AutotorchClient implements ClientModInitializer {
+public class AutoTorchClient implements ClientModInitializer {
+
     private MinecraftClient client;
     public ConfigHolder<ModConfig> CONFIG;
     private ModConfig CDATA;
     private static final ImmutableSet<Item> TorchSet = ImmutableSet.of(Items.TORCH, Items.SOUL_TORCH);
-
+    private static final KeyBinding.Category AUTOTORCH_CATEGORY = KeyBinding.Category.create(Identifier.of("autotorch", "main"));
     private static final KeyBinding AutoPlaceBinding = KeyBindingHelper.registerKeyBinding(
             new KeyBinding(
                     "autotorch.autotorch.toggle",
                     InputUtil.Type.KEYSYM,
                     GLFW.GLFW_KEY_LEFT_ALT,
-                    "category.autotorch.main"
+                    AUTOTORCH_CATEGORY
             )
     );
 
     @Override
     public void onInitializeClient() {
         this.client = MinecraftClient.getInstance();
-        CONFIG = AutoConfig.register(ModConfig.class, GsonConfigSerializer::new);
         ClientTickEvents.END_CLIENT_TICK.register(this::tick);
+        CONFIG = AutoConfig.register(ModConfig.class, GsonConfigSerializer::new);
         CDATA = CONFIG.getConfig();
         CONFIG.registerLoadListener((manager, data) -> {
             CDATA = data;
@@ -79,11 +62,15 @@ public class AutotorchClient implements ClientModInitializer {
         if (client.player != null && client.world != null) {
             if (AutoPlaceBinding.wasPressed()) {
                 CDATA.enabled = !CDATA.enabled;
-                var msg = CDATA.enabled ? Text.translatable("autotorch.message.enabled") : Text.translatable("autotorch.message.disabled");
-                client.player.sendMessage(msg, false);
+                var message = CDATA.enabled ? Text.translatable("autotorch.message.enabled") : Text.translatable("autotorch.message.disabled");
+                client.player.sendMessage(message, false);
             }
-            if (!CDATA.enabled) return;
-            if (!TorchSet.contains(client.player.getOffHandStack().getItem())) return;
+            if (!CDATA.enabled) {
+                return;
+            }
+            if (!TorchSet.contains(client.player.getOffHandStack().getItem())) {
+                return;
+            }
             BlockPos PlayerBlock = client.player.getBlockPos();
             if (client.world.getLightLevel(LightType.BLOCK, PlayerBlock) < CDATA.lightLevel && canPlaceTorch(PlayerBlock)) {
                 offHandRightClickBlock(PlayerBlock);
@@ -92,19 +79,17 @@ public class AutotorchClient implements ClientModInitializer {
     }
 
     private void offHandRightClickBlock(BlockPos pos) {
-        Vec3d hitVec = Vec3d.ofBottomCenter(pos);
+        Vec3d vec3d = Vec3d.ofBottomCenter(pos);
         if (CDATA.accuratePlacement) {
-            PlayerMoveC2SPacket.LookAndOnGround packet = new PlayerMoveC2SPacket.LookAndOnGround(client.player.getYaw(), 90.0F, true);
+            PlayerMoveC2SPacket.LookAndOnGround packet = new PlayerMoveC2SPacket.LookAndOnGround(client.player.getYaw(), 90.0F, true, false);
             client.player.networkHandler.sendPacket(packet);
         }
-        ActionResult one = client.interactionManager.interactBlock(client.player, Hand.OFF_HAND,
-                new BlockHitResult(hitVec, Direction.DOWN, pos, false));
-        ActionResult two = client.interactionManager.interactItem(client.player, Hand.OFF_HAND);
+        client.interactionManager.interactBlock(client.player, Hand.OFF_HAND, new BlockHitResult(vec3d, Direction.DOWN, pos, false));
+        client.interactionManager.interactItem(client.player, Hand.OFF_HAND);
     }
 
     public boolean canPlaceTorch(BlockPos pos) {
-        return (client.world.getBlockState(pos).getFluidState().isEmpty() &&
-                Block.sideCoversSmallSquare(client.world, pos.down(), Direction.UP));
+        return (client.world.getBlockState(pos).getFluidState().isEmpty() && Block.sideCoversSmallSquare(client.world, pos.down(), Direction.UP));
     }
+
 }
-   
--- src/main/java/autotorch/autotorch/client/ModConfig.java
+++ src/main/java/autotorch/autotorch/client/ModConfig.java
@@ -2,24 +2,18 @@ package autotorch.autotorch.client;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.*;
-import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
-@Config(name="autotorch")
+@Config(name = "autotorch")
 public class ModConfig implements ConfigData {
 
-    @Comment("Enable the Auto Torch Mod")
+    @ConfigEntry.Gui.Tooltip
     boolean enabled = true;
 
-    @Comment("Enable accurate torch placement directly on block below.\n" +
-            "Note: This may be considered cheaty because it 'fakes' player rotation.\n" +
-            "And is basically desyncing the client and server\n" +
-            "Enable only if you are sure your server doesn't consider it cheaty.")
+    @ConfigEntry.Gui.Tooltip
     boolean accuratePlacement = false;
 
-    @Comment("The block light level below which the torches are placed.")
+    @ConfigEntry.Gui.Tooltip
     @ConfigEntry.BoundedDiscrete(min = 1, max = 14)
     int lightLevel = 4;
-
-
 
 }
